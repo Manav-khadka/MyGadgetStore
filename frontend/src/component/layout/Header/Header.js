d8a60d7f2b4af72f0b750214c2798e9a81b3984f
Manav Khadka <manavkhadka2004@gmai.com>
2022-10-05 08:12:15 +0530

    frontend updated

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -1,17 +1,45 @@
 import React from "react";
 import { ReactNavbar } from "overlay-navbar";
-import {FaUserAlt,FaSearch} from "react-icons/fa";
-
+import { FaUser, FaSearch, FaCartPlus } from "react-icons/fa";
+import logo from "../../../images/logo.png";
 
 const Header = () => {
-  return (<ReactNavbar
-    searchIcon={true}
-    SearchIconElement={FaSearch}
-    // searchIconSize="2vmax"
-    // searchIconMargin="0 0 0 0"
-    profileIcon={true}
-    ProfileIconElement={FaUserAlt}
-  />
+  return (
+    <ReactNavbar
+      burgerColorHover="#601ce3"
+      link1ColorHover="#601ce3"
+
+      logo={logo}
+      logoWidth="10vmax"
+      navColor1="#fff"
+      link1Margin="1.5vmax"
+      link1Text="Home"
+      link2Text="Products"
+      link3Text="Contact"
+      link4Text="About"
+      link1Url="/"
+      link2Url="/products"
+      link3Url="/contact"
+      link4Url="/about"
+      link1Size="1.6vmax"
+      link1Color="rgba(35, 35, 35,0.8)"
+      // nav1justifyContent= "flex-end"
+      nav2justifyContent="flex-end"
+      nav3justifyContent="flex-start"
+      nav4justifyContent="flex-start"
+      profileIcon={true}
+      ProfileIconElement={FaUser}
+      profileIconSize="2.0vmax"
+      profileIconUrl="/profile"
+      searchIcon={true}
+      SearchIconElement={FaSearch}
+      cartIcon={true}
+      CartIconElement={FaCartPlus}
+      cartIconMargin="1vmax"
+      profileIconColor="rgba(35, 35, 35,0.8)"
+      searchIconColor="rgba(35, 35, 35,0.8)"
+      cartIconColor="rgba(35, 35, 35,0.8)"
+    />
   );
 };
 export default Header;
